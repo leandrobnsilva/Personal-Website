4c6c239f224ce1b0cd339294590da9154b16a77e
leandrobnsilva <leandrobnsilva@gmail.com>
2019-04-11 09:50:35 -0300

    Update index page text and styles.

--- pages/index.php
+++ pages/index.php
@@ -1,12 +1,12 @@
 <div class="intro">
-	<div class="title center">What's up?</div>
-	<div class="text center">My name's Leandro Boari and I'm graduating in <a target="blank" href="http://www.deg.ufla.br/engenharia-de-controle-e-automacao/">Control and Automation Engineering</a> at the <a target="blank" href="https://www.ufla.br">Federal University of Lavras, UFLA</a>.</div>
+	<div class="title center">Hi, I'm Leandro!</div>
+	<div class="text center">I develop electronic devices, little robots, beautifull codes and many interesting things. I'm undergraduating in <a target="blank" href="http://www.deg.ufla.br/engenharia-de-controle-e-automacao/">Control and Automation Engineering</a> at the <a target="blank" href="https://www.ufla.br">Federal University of Lavras, UFLA</a>.</div>
 	<div class="avatar"></div>
 	<div class="info">
 		<div class="each">
 			<div class="name">Name</div>
 			<div class="icon fa fa-pencil"></div>
-			<div class="value">Leandro Boari</div>
+			<div class="value">Leandro Boari Naves Silva</div>
 		</div>
 		<div class="each">
 			<div class="name">Live in</div>
@@ -24,9 +24,9 @@
 			<div class="value">contato@leandro.xyz</div>
 		</div>
 		<div class="each">
-			<div class="name">Website</div>
-			<div class="icon fa fa fa-link"></div>
-			<div class="value"><a href="http://leandro.xyz">leandro.xyz</a></div>
+			<div class="name">Email</div>
+			<div class="icon fa fa fa-envelope"></div>
+			<div class="value">contato@leandro.xyz</div>
 		</div>
 	</div>
 </div>
